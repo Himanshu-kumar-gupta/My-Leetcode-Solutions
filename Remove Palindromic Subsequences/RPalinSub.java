877d4ef687d074c384704729089303a9cc9a4c37
Himanshu Kumar Gupta <60381467+Himanshu-kumar-gupta@users.noreply.github.com>
2022-06-08 09:51:35 +0530

    Complete Solution.
    https://leetcode.com/problems/remove-palindromic-subsequences/

---
+++ Remove Palindromic Subsequences/RPalinSub.java
@@ -0,0 +1,22 @@
+public class RPalinSub {
+    public int removePalindromeSub(String s) {
+         StringBuilder s1=new StringBuilder(s);
+
+         // The solution can be either '1' or '2'
+         // 0 is not an option as '1 <= s.length <= 1000'
+
+         // The string can be palindrome so the solution is 1
+         s1.reverse();
+         if (s1.toString()==s)
+            return 1;
+
+         // All the a's can be removed in 1st step and all the b's can be removed in 2nd step
+         // So the max solution can be 2
+         return 2;
+    }
+
+    public static void main(String[] args) {
+        RPalinSub obj = new RPalinSub();
+        System.out.println(obj.removePalindromeSub("abb"));
+    }
+}
